879d809103c0fbd3fc5694b6374d0487ff94c52c
user2365 <73748621+user2365@users.noreply.github.com>
2021-10-02 12:42:43 +0530

    Average 2 numbers in python

---
+++ Average of 2 numbers in python.py
@@ -0,0 +1,8 @@
+#Average 2 numbers in python
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+average =(int(num1) + int(num2))
+
+print('average:{0} '.format(average))
